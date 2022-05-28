6b915b99a82634f583efd937d3f2daadeb3043a4
CarlosJaBr <carjabo12@gmail.com>
2022-05-28 11:15:31 -0500

    Screen Menu, Register and Score has been added

---
+++ src/controller/MenuController.java
@@ -0,0 +1,25 @@
+package controller;
+
+import application.Main;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class MenuController {
+	
+	
+	private Main main;
+	
+	@FXML
+    public void startGame(ActionEvent event) {
+		main.showRegisterView();
+    }
+	
+	public void setMain(Main main) {
+		this.main = main; 
+	}
+	
+	@FXML
+	public void lookScores(ActionEvent event) {
+		main.showScoreView();
+	}
+}
